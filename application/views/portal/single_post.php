2066e494ca856370cdd9a41d135c79b4ca690b07
adityoqrren <aji.adityo8@gmail.com>
2019-02-16 23:52:16 +0700

    Perubahan partial

---
+++ application/views/portal/single_post.php
@@ -0,0 +1,97 @@
+<section class="ptb-0">
+  <div class="mb-30 brdr-ash-1 opacty-5"></div>
+  <div class="container">
+    <a class="mt-10" href="index.html"><i class="mr-5 ion-ios-home"></i>Home<i class="mlr-10 ion-chevron-right"></i></a>
+    <a class="mt-10" href="archive-page.html">Blog Archive<i class="mlr-10 ion-chevron-right"></i></a>
+    <a class="mt-10 color-ash" href="single-page.html">Single Blog</a>
+  </div><!-- container -->
+</section>
+
+
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-lg-8 offset-md-2">
+        <img src="<?= base_url('assets/portal/images/myslider.jpg') ?>" alt="">
+        <h3 class="mt-30"><b>Peter Thiels VC Found Invests Million into Bitcoin, Market Reacts</b></h3>
+        <ul class="list-li-mr-20 mtb-15">
+          <li>by <a href="#"><b>Olivia Capzallo </b></a> Jan 25, 2018</li>
+          <li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+          <li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>30</li>
+        </ul>
+
+        <p>RSK is launching its bitcoins smart contracts platform in beta today.</p>
+
+        <p class="mtb-15">Formerly known as Rootstock, the startup has long been lauded for Rs potential to pave the way for the
+          implementation of ethereum-style smart contracts on broom. something enthusiasts believe vein keep the wodd's
+          largest cryptocurrency competitive with the platform t hnl acquably pionverecl
+          the idea I hat mote executing code could be rim on a bickchain.</p>
+
+        <p class="mtb-15">But while it would be easy enough for Wan users that want more complex smart contracts oo merely
+          some users believe that. as tdcoin is the largest and most secure cryptocumency, more experimental
+          features that debut on other networks will eventually make hal in
+          doing to, Uxy can r apitalier impressive startup infrastructure and serve different users.</p>
+
+        <p class="mtb-15">Yet.RSK's version of the functionality doesn't quite upgrade the bmcom blockchain itself. I he
+          capability will rather be brought to brtcoin via a sidechain. which moves tokens from the main btroin
+          blockchaln to a compatible network operated with the help of 25 companies.</p>
+
+        <p class="mtb-15">Still, RSK's CEO Diego Gutierrez 7aldivar believes the advance will effectively provide
+          the level of ubuty to potential users. </p>
+
+        <div class="quote-primary mtb-20">
+          <h5>"This Is the first time that there's going to be a smart contract platform powered by the bitcoin network."</h5>
+          <h5 class="mt-15"><b>Oliver Dale</b></h5>
+        </div><!-- quote-primary -->
+
+        <h4><b>How to Buy Powerledger POWR</b></h4>
+
+        <p class="mtb-15">You are not able to purchase POWR with "Fiat" currency so you will need to first purchase another
+          currency - the easiest to buy are Bitcoin or Ethereum which you can do at Coinbase using a bank transfer or
+          debit / credit card purchase and then swap that for POWR at an exchange such as Binance.</p>
+
+        <p class="mtb-15">You will have to carry out some identity verification when signing up as they have
+          to adhere to strict financial guidelines. Make sure you use our link to signup you will be
+          credited with $10 in free bitcoin when you make your first purchase of $100 </p>
+
+        <div class="float-left-right text-center mt-40 mt-sm-20">
+
+
+        </div><!-- float-left-right -->
+
+
+        <h4 class="p-title mt-50"><b>YOU MAY ALSO LIKE</b></h4>
+        <div class="row">
+
+          <div class="col-sm-6">
+            <img src="<?= base_url('assets/portal/images/crypto-news-2-600x450.jpg') ?>" alt="">
+            <h4 class="pt-20"><a href="#"><b>2017 Market Performance: <br/>Crypto vs.Stock</b></a></h4>
+            <ul class="list-li-mr-20 pt-10 mb-30">
+              <li class="color-lite-black">by <a href="#" class="color-black"><b>Olivia Capzallo,</b></a>
+              Jan 25, 2018</li>
+              <li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+              <li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>47</li>
+            </ul>
+          </div><!-- col-sm-6 -->
+
+          <div class="col-sm-6">
+              <img src="<?= base_url('assets/portal/images/crypto-news-2-600x450.jpg') ?>" alt="">
+            <h4 class="pt-20"><a href="#"><b>2017 Market Performance: Crypto vs.Stock</b></a></h4>
+            <ul class="list-li-mr-20 pt-10 mb-30">
+              <li class="color-lite-black">by <a href="#" class="color-black"><b>Olivia Capzallo,</b></a>
+              Jan 25, 2018</li>
+              <li><i class="color-primary mr-5 font-12 ion-ios-bolt"></i>30,190</li>
+              <li><i class="color-primary mr-5 font-12 ion-chatbubbles"></i>47</li>
+            </ul>
+          </div><!-- col-sm-6 -->
+
+        </div><!-- row -->
+
+      </div><!-- col-md-9 -->
+
+      <div class="d-none d-md-block d-lg-none col-md-3"></div>
+
+    </div><!-- row -->
+
+  </div><!-- container -->
+</section>
--- application/views/portal/template/navbar.php
+++ application/views/portal/template/navbar.php
@@ -1,59 +1,35 @@
-
 <header>
-	<!--	<div class="bg-191">
-			<div class="container">
-				<div class="oflow-hidden color-ash font-9 text-sm-center ptb-sm-5">
+	<div class="container">
+		<a class="logo" href="index.html"><img src="<?= base_url('assets/portal/images/logo-black.png') ?>" alt="Logo"></a>
+		<a class="right-area src-btn" href="#" >
+			<i class="active src-icn ion-search"></i>
+			<i class="close-icn ion-close"></i>
+		</a>
+		<div class="src-form">
+			<form>
+				<input type="text" placeholder="Search here">
+				<button type="submit"><i class="ion-search"></i></a></button>
+			</form>
+		</div><!-- src-form -->
 
-					<ul class="float-left float-sm-none list-a-plr-10 list-a-plr-sm-5 list-a-ptb-15 list-a-ptb-sm-10">
-						<li><a class="pl-0 pl-sm-10" href="#">About</a></li>
-						<li><a href="#">Advertise</a></li>
-						<li><a href="#">Submit Press Release</a></li>
-						<li><a href="#">Contact</a></li>
-					</ul>
-					<ul class="float-right float-sm-none list-a-plr-10 list-a-plr-sm-5 list-a-ptb-15 list-a-ptb-sm-5">
-						<li><a class="pl-0 pl-sm-10" href="#"><i class="ion-social-facebook"></i></a></li>
-						<li><a href="#"><i class="ion-social-twitter"></i></a></li>
-						<li><a href="#"><i class="ion-social-google"></i></a></li>
-						<li><a href="#"><i class="ion-social-instagram"></i></a></li>
-						<li><a href="#"><i class="ion-social-bitcoin"></i></a></li>
-					</ul>
+		<a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
 
-				</div><!-- top-menu
-			</div><!-- container
-		</div><!-- bg-191 -->
-
-		<div class="container">
-			<a class="logo" href="index.html"><img src="<?php base_url('assets/portal/images/logo-black.png') ?>" alt="Logo"></a>
-
-			<a class="right-area src-btn" href="#" >
-				<i class="active src-icn ion-search"></i>
-				<i class="close-icn ion-close"></i>
-			</a>
-			<div class="src-form">
-				<form>
-					<input type="text" placeholder="Search here">
-					<button type="submit"><i class="ion-search"></i></a></button>
-				</form>
-			</div><!-- src-form -->
-
-			<a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
-
-			<ul class="main-menu" id="main-menu">
-				<li><a href="02_archive-page.html">BERANDA</a></li>
-				<li class="drop-down"><a href="03_single-post.html">PROFIL<i class="ion-arrow-down-b"></i></a>
-					<ul class="drop-down-menu drop-down-inner">
-						<li><a href="#">SEJARAH LPPM</a></li>
-						<li><a href="#">LPPM</a></li>
-						<li><a href="#">STRUKTUR ORGANISASI</a></li>
-						<li><a href="#">VISI DAN MISI</a></li>
-						<li><a href="#">RSP</a></li>
-						<li><a href="#">RENSTRA</a></li>
-					</ul>
-				</li>
-				<li><a href="04_FAQS.html">KONTAK</a></li>
-			<!--<li><a href="05_FAQS-single.html">EXPLAINED</a></li>
-				<li><a href="06_contact-us.html">ICON CLAENDER</a></li>-->
-			</ul>
-			<div class="clearfix"></div>
-		</div><!-- container -->
-	</header>
+		<ul class="main-menu" id="main-menu">
+			<li><a href="02_archive-page.html">BERANDA</a></li>
+			<li class="drop-down"><a href="03_single-post.html">PROFIL<i class="ion-arrow-down-b"></i></a>
+				<ul class="drop-down-menu drop-down-inner">
+					<li><a href="#">SEJARAH LPPM</a></li>
+					<li><a href="#">LPPM</a></li>
+					<li><a href="#">STRUKTUR ORGANISASI</a></li>
+					<li><a href="#">VISI DAN MISI</a></li>
+					<li><a href="#">RSP</a></li>
+					<li><a href="#">RENSTRA</a></li>
+				</ul>
+			</li>
+			<li><a href="04_FAQS.html">KONTAK</a></li>
+		<!--<li><a href="05_FAQS-single.html">EXPLAINED</a></li>
+			<li><a href="06_contact-us.html">ICON CLAENDER</a></li>-->
+		</ul>
+		<div class="clearfix"></div>
+	</div><!-- container -->
+</header>
